5c2c6a0936ec0607ba1cc312a06062ee621cae68
Janadriel <pjanadriel@gmail.com>
2024-11-04 18:38:44 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Jan Adviel T. Pilayan";
+let age = 19;
+let favoriteNumber = 31;
+let favoriteColor = "black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
